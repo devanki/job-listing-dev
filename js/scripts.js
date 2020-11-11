448d00f9a24d047aeabaebf68e4b60fdc31b68bf
Anna <siek.anna86@gmail.com>
2020-11-11 10:53:11 +0100

    remove filter / check filter

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,45 @@ let filterArray = document.getElementsByClassName('js-filters__item');
 
 
 function addFilterHandler(e) { 
+
     e.preventDefault();
-    let filterElement = e.target.getAttribute('data-filter');
-    let filterElementTitle = e.target.textContent; 
-    // console.log(e.target);
+    const filterElement = e.target.getAttribute('data-filter');
+    const filterElementTitle = e.target.textContent; 
     let newFilterElement = document.createElement('div');
 
-    newFilterElement.classList.add('js-filters__item','filters__item')
+    newFilterElement.classList.add('js-filters__item','filters__item');
     newFilterElement.setAttribute('data-filter', filterElement);
     newFilterElement.textContent = filterElementTitle;
     filtersList.appendChild(newFilterElement);
+    newFilterElement.onclick = removeFilterHandler;
+
 };
 
 function removeFilterHandler(el) {
     let element = el.target;
-    console.log(element);
     element.remove();
+}
+
+function checkIfExist() {
+     //check if filter already exist
+     const filters = document.querySelectorAll('.js-filters__item');
+     // console.log(filters);
+     
+     // [...filters].forEach( (filter) => {
+     //     if ( filter.getAttribute('data-filter') === filterElement) {
+     //         console.log('filter already exist');
+     //         console.log( filter.getAttribute('data-filter'));
+     //     } else {
+     //         console.log('add new filter');
+     //         
+     //     }
+     // });
+ 
+    //  if ( [...filters].filter(element =>  element.dataset.filter == filterElement)) {
+    //      console.log(filterElement);
+    //  } else {
+    //      console.log('add new filter');
+    //  }
 }
 
 Array.from(filterBtnArray).forEach((btn) => {
